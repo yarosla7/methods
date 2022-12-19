d9d9ea76bf1cd07704778b67bab7744f859f68d9
y.yanin <yanin.yarosla7@gmail.com>
2022-12-19 19:55:06 +0300

    homework_is_done_1.3 final fix.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void printLeapYear(int year) {
 
-        if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным.");
         } else {
             System.out.println(year + " год не является високосным.");
